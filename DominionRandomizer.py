a05d8341fe97f1e2cfe04d933a20b5bf8ee0c326
Adrienne <adrie.k@gmail.com>
2013-08-21 16:49:54 -0700

    + Expansion
    Added the option to randomize either Dominion, Intrigue, or the Seaside
    expansion.

--- DominionRandomizer.py
+++ DominionRandomizer.py
@@ -2,11 +2,34 @@
 
 import random
 
-print 'Time to play Dominion'
-
-unshuffled_list = (['Cellar', 'Chapel', 'Moat', 'Chancellor', 'Village', 'Woodcutter', 'Workshop', 'Bureaucrat', 'Feast', 'Gardens', 'Militia', 'Moneylender', 'Remodel', 'Smithy', 'Spy', 'Thief', 'Throne Room', 'Council Room', 'Festival', 'Laboratory', 'Library', 'Market', 'Mine', 'Witch', 'Adventurer'])
-
 shuffled_list = []
+unshuffled_list = []
+
+print '\n' + 'Time to play Dominion' + '\n' + '\n' + 'Which expansion would you like to use?' + '\n' + "	Dominion" + '\n' + "	Intrigue" + '\n' + "	Seaside" + '\n'
+
+while True:
+	deck = raw_input()
+
+	if deck == 'Dominion':
+		break
+
+	elif deck == 'Intrigue':
+		break
+
+	elif deck == 'Seaside':
+		break
+
+	else:
+		print "Try that again." + '\n'
+
+if deck == "Dominion":
+	unshuffled_list = (['Cellar', 'Chapel', 'Moat', 'Chancellor', 'Village', 'Woodcutter', 'Workshop', 'Bureaucrat', 'Feast', 'Gardens', 'Militia', 'Moneylender', 'Remodel', 'Smithy', 'Spy', 'Thief', 'Throne Room', 'Council Room', 'Festival', 'Laboratory', 'Library', 'Market', 'Mine', 'Witch', 'Adventurer'])
+
+elif deck == "Intrigue":
+	unshuffled_list = (['Courtyard','Pawn','Secret', 'Chamber','Great Hall','Masquerade','Shanty Town','Steward','Swindler','Wishing Well','Baron','Bridge','Conspirator','Coppersmith','Ironworks','Mining Village','Scout','Duke','Minion','Saboteur','Torturer','Trading Post','Tribute','Upgrade','Harem','Nobles'])
+
+elif deck == "Seaside":
+	unshuffled_list = (['Embargo','Haven','Lighthouse','Native Village','Pearl Diver','Ambassador','Fishing Village','Lookout','Smugglers','Warehouse','Caravan','Cutpurse','Island','Navigator','Pirate Ship','Salvager','Sea Hag','Treasure Map','Bazaar','Explorer','Ghost Ship','Merchant Ship','Outpost','Tactician','Treasury','Wharf'])
 
 int = 10
 
